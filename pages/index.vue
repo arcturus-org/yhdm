77cdbc544d5599e7c46c6fc0a3e5d11bc5ddc118
ARCTRUS <1638330246@qq.com>
2023-02-18 21:34:48 +0800

    feat: update home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,14 @@
 <template>
-  <div
-    v-if="loading"
-    class="d-flex justify-center align-center"
-    style="flex-grow: 1;"
-  >
+  <div v-if="loading" class="loading">
     <v-progress-circular indeterminate color="#1E88E5"></v-progress-circular>
   </div>
 
   <div v-else class="pa-4">
-    <Week />
-    <Anime type="japan" title="日本动漫"/>
-    <Anime type="china" title="国产动漫"/>
-    <Anime type="usa" title="美国动漫"/>
-    <Anime type="movies" title="动漫电影"/>
+    <nx-week />
+    <nx-anime type="japan" title="日本动漫" />
+    <nx-anime type="china" title="国产动漫" />
+    <nx-anime type="usa" title="美国动漫" />
+    <nx-anime type="movies" title="动漫电影" />
   </div>
 </template>
 
@@ -26,6 +22,6 @@ const $store = useStore();
 const { loading } = storeToRefs($store);
 
 onMounted(() => {
-  $store.getHomeData();
+  $store.homeData();
 });
 </script>
