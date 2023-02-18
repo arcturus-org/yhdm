3b03633719db593d3751c3a94a08bf5bc010e003
ARCTRUS <1638330246@qq.com>
2023-02-18 20:34:01 +0800

    feat: change import path

--- server/api/home.ts
+++ server/api/home.ts
@@ -1,9 +1,12 @@
 import { defineEventHandler } from 'h3';
 import { useRuntimeConfig } from '#imports';
-import { homeResolver } from '../resolver';
+import { homeResolver } from '@server/resolver';
 
+// obtains home page data and processes it
 export default defineEventHandler(async () => {
-  const res: string = await $fetch('/', { baseURL: useRuntimeConfig().public.api });
+  const res: string = await $fetch('/', {
+    baseURL: useRuntimeConfig().public.api,
+  });
 
   return homeResolver(res);
 });
