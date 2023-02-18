bf33b0983f68d22d729fd578823aa09d0fb22fc0
ARCTRUS <1638330246@qq.com>
2023-02-18 20:33:35 +0800

    feat: add a function to obtain color

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,18 @@
-export function matchVideoId(url: string): string {
+export const videoId = (url: string): string => {
   const re = /\/view\/(?<id>\d+)\.html/;
 
   return re.exec(url)!.groups!.id;
-}
+};
+
+export const levelColor = (level: number) => {
+  switch (level) {
+    case 0:
+      return '#dd2c00';
+    case 1:
+      return '#ff6d00';
+    case 2:
+      return '#ffab00';
+    default:
+      return '#03dac6';
+  }
+};
