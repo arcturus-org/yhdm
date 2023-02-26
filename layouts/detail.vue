b5495a6ccfe3bd061b1ec6ac4c9524fd2dc0e863
ARCTRUS <1638330246@qq.com>
2023-02-26 19:29:04 +0800

    feat: add a button to back

--- layouts/detail.vue
+++ layouts/detail.vue
@@ -1,5 +1,10 @@
 <template>
   <v-layout full-height>
+    <v-app-bar :elevation="0" class="w-100" density="compact">
+      <template v-slot:prepend>
+        <v-btn icon="mdi-arrow-left" @click="() => $router.back()"></v-btn>
+      </template>
+    </v-app-bar>
     <v-main>
       <slot />
     </v-main>
