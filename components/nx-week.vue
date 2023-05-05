a7956b235c0d159297f5fbf9e08d3cb7821840d6
ARCTRUS <1638330246@qq.com>
2023-05-05 21:33:43 +0800

    feat: Change some styles

--- components/nx-week.vue
+++ components/nx-week.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-tabs v-model="tab" align-tabs="start" density="compact" color="#1E88E5">
-    <v-tab v-for="(t, i) in tabs" :key="t" :value="i" class="tab">
-      {{ t }}
+  <v-tabs v-model="tab" density="compact" color="#1E88E5">
+    <v-tab v-for="(tab, idx) in tabs" :key="`tab-${idx}`" :value="idx" class="tab">
+      {{ tab }}
     </v-tab>
   </v-tabs>
 
   <v-window v-model="tab">
-    <v-window-item v-for="(t, i) in tabs" :key="t" :value="i">
+    <v-window-item v-for="(t, i) in tabs" :key="t" :value="i" class="week-container">
       <v-list class="d-flex flex-column flex-wrap week-list" density="compact">
         <nx-list-item class="week-list-item" v-for="(item, idx) in week[i]" :key="idx" :content="item" />
       </v-list>
@@ -17,19 +17,20 @@
 <script setup lang="ts">
 import { ref } from '#imports';
 
-defineProps<{
-  week: WList;
-  today: number;
-}>();
+defineProps<{ week: WList }>();
 
 const tabs = ['周一', '周二', '周三', '周四', '周五', '周六', '周日'];
 
-const tab = ref(1);
+const tab = ref((new Date().getDay() + 6) % 7);
 </script>
 
 <style scoped lang="scss">
 .tab {
   min-width: 36px !important;
+}
+
+:deep(.v-list) {
+  padding: 0;
 }
 
 @media screen and (max-width: 600px) {
@@ -39,8 +40,12 @@ const tab = ref(1);
 }
 
 @media screen and (min-width: 600px) {
-  .week-list {
-    height: 320px;
+  .week-container {
+    height: 240px;
+
+    .week-list {
+      height: inherit;
+    }
 
     .week-list-item {
       width: 33.3%;
