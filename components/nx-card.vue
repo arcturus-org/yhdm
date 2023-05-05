b5e1ab829aee3b9ed037f0df5249ec7aa5782602
ARCTRUS <1638330246@qq.com>
2023-05-05 20:59:40 +0800

    feat: Change height of image

--- components/nx-card.vue
+++ components/nx-card.vue
@@ -1,21 +1,11 @@
 <template>
-  <v-card :elevation="0" hover :href="`/view/${content.id}`">
-    <v-img
-      :src="content.cover"
-      class="align-end text-white image"
-      :height="height ?? 240"
-      cover
-    >
+  <v-card :href="`/view/${content.id}`">
+    <v-img :src="content.cover" class="align-end text-white image" :height="height ?? 200" cover>
       <div class="mask"></div>
 
       <div class="mx-2 status">{{ content.status }}</div>
 
-      <v-chip
-        color="secondary"
-        variant="tonal"
-        size="x-small"
-        class="mb-2 mx-2"
-      >
+      <v-chip color="secondary" variant="tonal" size="x-small" class="mb-2 mx-2">
         {{ content.score }}
       </v-chip>
 
@@ -25,7 +15,7 @@
     </v-img>
 
     <v-card-item>
-      <v-card-title :style="{ fontSize: 15 }">{{ content.name }}</v-card-title>
+      <v-card-title style="font-size: 14px">{{ content.name }}</v-card-title>
       <v-card-subtitle> 主演: {{ content.actors.join(',') }} </v-card-subtitle>
     </v-card-item>
   </v-card>
@@ -50,15 +40,18 @@ defineProps<{
     bottom: 0;
     background: linear-gradient(
       to bottom,
-      rgba(0, 0, 0, 0.8) 0%,
-      rgba(255, 255, 255, 0) 50%,
-      rgba(0, 0, 0, 0.8) 100%
+      rgba(20, 20, 20, 0.8) 0,
+      rgba(89, 89, 89, 0.6) 25%,
+      rgba(191, 191, 191, 0.5) 50%,
+      rgba(89, 89, 89, 0.6) 75%,
+      rgba(20, 20, 20, 0.8) 100%
     );
   }
 
   .status {
     position: absolute;
-    top: 0;
+    top: 5px;
+    font-size: 12px;
   }
 }
 </style>
